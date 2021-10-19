129c06c91fd21327b5ad5761551414fe0a071885
Stéphanie Antignac <antignacstephanie17@gmail.com>
2021-10-19 09:20:03 +0200

    Creation des fichiers JS et HTML

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+    return (
+        <div className={"Compteur"}>
+            <div>
+                <p>{`00:00:00`}</p>
+            </div>
+        </div>
+    );
+}
+export default App;
--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-// index.js
-import React from "react"
-import ReactDOM from "react-dom"
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
 
-ReactDOM.render(<h1>Hello world!</h1>, document.getElementById("root"))
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
